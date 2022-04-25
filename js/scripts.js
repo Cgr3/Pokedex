59954db1da38518a10c4cbc47e2c32e6bbb72f2e
Cgr3 <102884458+Cgr3@users.noreply.github.com>
2022-04-25 12:30:07 -0400

    Major changes to scripts.js page
    Removed all code to start
    Added a variable for pokemon
    Added objects for pokemon in an Array
    Pokemon added - Raichu, Dragonite, Blaziken & Alakazam

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,26 @@
-//this is a comment
-
-/* this is a
-multi line comment*/
-
-let favoriteFood = 'Spaghetti';
-document.write(favoriteFood);
-alert('hello world');
+let pokemonList = [
+  {
+    name: 'Alakazam',
+    height: 1.5,
+    type: 'Psychic',
+    evolutions: ['Abra', 'Kadabra', 'Alakazam']
+  },
+  {
+    name: 'Dragonite',
+    height: 2.2,
+    type: ['Dragon', 'Flying'],
+    evolutions: ['Dratini', 'Dragonair', 'Dragonite']
+  },
+  {
+    name: 'Blaziken',
+    height: 1.9,
+    type: ['Fire', 'Fighting'],
+    evolutions: ['Torchic', 'Combusken', 'Blaziken']
+  },
+  {
+    name: 'Raichu',
+    height: .8,
+    type: 'Electric',
+    evolutions: ['Pichu', 'Pikachu', 'Raichu']
+  }
+];
