67f762d2088e2a5e68d44d2018b9313c005987fe
Cgr3 <102884458+Cgr3@users.noreply.github.com>
2022-05-02 10:35:17 -0400

    Changed the loop used to call pokemon
    Removed the for loop and replaced with forEach loop!
    Fixed spacing on evolutions

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,41 @@
-let pokemonList = [
-  // pokemon height is in meters
-  {
-    name: 'Alakazam',
-    height: 1.5,
-    type: 'Psychic',
-    evolutions: ['Abra', 'Kadabra', 'Alakazam']
-  },
-  {
-    name: 'Dragonite',
-    height: 2.2,
-    type: ['Dragon', 'Flying'],
-    evolutions: ['Dratini', 'Dragonair', 'Dragonite']
-  },
-  {
-    name: 'Blaziken',
-    height: 1.9,
-    type: ['Fire', 'Fighting'],
-    evolutions: ['Torchic', 'Combusken', 'Blaziken']
-  },
-  {
-    name: 'Raichu',
-    height: .8,
-    type: 'Electric',
-    evolutions: ['Pichu', 'Pikachu', 'Raichu']
-  }
-];
+  let pokemonList = [
+    // pokemon height is in meters
+    {
+      name: 'Alakazam',
+      height: 1.5,
+      type: 'Psychic',
+      evolutions: ['Abra', ' Kadabra', ' Alakazam']
+    },
+    {
+      name: 'Dragonite',
+      height: 2.2,
+      type: ['Dragon', 'Flying'],
+      evolutions: ['Dratini', ' Dragonair', ' Dragonite']
+    },
+    {
+      name: 'Blaziken',
+      height: 1.9,
+      type: ['Fire', 'Fighting'],
+      evolutions: ['Torchic', ' Combusken', ' Blaziken']
+    },
+    {
+      name: 'Raichu',
+      height: .8,
+      type: 'Electric',
+      evolutions: ['Pichu', ' Pikachu', ' Raichu']
+    }
+  ];
+
+pokemonList.forEach(function(pokemon) {
+  document.write(pokemon.name + ': ' + pokemon.height + ' ' + pokemon.type + ' ' + pokemon.evolutions + ' ')
+});
 
 // For loop to list all pokemon name and height in pokemonList
-for (i = 0; i < pokemonList.length; i++) {
-  // If else statement to determine if the height of the pokemon is over 2
-  if (pokemonList[i].height > 2) {
-    document.write(pokemonList[i].name + ": (" + pokemonList[i].height + ") " + " - Wow that's a LARGE Pokemon! ")
-  } else {
-    document.write(pokemonList[i].name + ": (" + pokemonList[i].height + ") " );
-  }
-}
+//for (i = 0; i < pokemonList.length; i++) {
+//  // If else statement to determine if the height of the pokemon is over 2
+//  if (pokemonList[i].height > 2) {
+//    document.write(pokemonList[i].name + ": (" + pokemonList[i].height + ") " + " - Wow that's a LARGE Pokemon! ")
+//  } else {
+//    document.write(pokemonList[i].name + ": (" + pokemonList[i].height + ") " );
+//  }
+//}
